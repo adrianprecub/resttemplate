3713c7b61b4e324001f88844c8d11e94e3ac1ea7
Adi <adi.precub@gmail.com>
2018-05-20 22:33:05 +0300

    add customizer

---
+++ src/main/java/com/resttemplate/CustomRestTemplateCustomizer.java
@@ -0,0 +1,14 @@
+package com.resttemplate;
+
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by adi on 5/20/18.
+ */
+public class CustomRestTemplateCustomizer implements RestTemplateCustomizer {
+    @Override
+    public void customize(RestTemplate restTemplate) {
+
+    }
+}
--- src/main/java/com/resttemplate/HelloController.java
+++ src/main/java/com/resttemplate/HelloController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,14 @@ public class HelloController {
 
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    @Autowired
-    @Qualifier(value = "default")
     private RestTemplate restTemplate;
+
+    @Autowired
+    public HelloController(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
+
+
 
     @RequestMapping("/")
     public String index() {
